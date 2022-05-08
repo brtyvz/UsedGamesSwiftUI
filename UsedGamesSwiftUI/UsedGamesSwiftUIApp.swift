a37db154ecbab284e04b988be70165a4e287e6cb
Berat Yavuz <beratttyavuzz@gmail.com>
2022-05-08 23:07:37 +0300

    Initial Commit

---
+++ UsedGamesSwiftUI/UsedGamesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesSwiftUIApp.swift
+//  UsedGamesSwiftUI
+//
+//  Created by Berat Yavuz on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
